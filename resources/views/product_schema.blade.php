7f3ef0ea78208b5eb0e6b6635bb2d548f600214c
silehage <silehwage@gmail.com>
2022-10-01 14:02:59 +0700

    fix schema validation

--- resources/views/product_schema.blade.php
+++ resources/views/product_schema.blade.php
@@ -5,19 +5,19 @@ $data = $jsapp['page']['product_schema']
 {
   "@context": "https://schema.org",
   "@type": "Product",
-  "aggregateRating": {
-    "@type": "AggregateRating",
-    "ratingValue": {{ $data['rating'] }},
-    "reviewCount": {{ $data['reviews_count'] }},
-  },
-  "description": {{ $data['description'] }},
-  "name": {{ $data['name'] }},
-  "image": {{ $data['image'] }},
+  "description": "{{ $data['description'] }}",
+  "name": "{{ $data['name'] }}",
+  "image": "{{ $data['image'] }}",
   "offers": {
     "@type": "Offer",
     "availability": "https://schema.org/InStock",
     "price": {{ $data['price'] }},
     "priceCurrency": "IDR"
+  },
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "{{ $data['rating'] }}",
+    "reviewCount": "{{ $data['reviews_count'] }}"
   }
 }
 </script>
